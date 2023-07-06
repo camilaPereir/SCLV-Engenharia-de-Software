b7831b4632d241b1b5deb1d9885a9ce492ca2838
Camila Pereira de Oliveira <camila379.cp@gmail.com>
2023-07-05 21:29:04 -0300

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <>
       <footer className={`fixed-bottom bg-body-tertiary ${tema === "dark" ? "text-light" : "text-dark"} text-center p-3`}>
-        Copyright &copy; Camila Pereira 2023 <br />
+        Copyright &copy; Camila Pereira e Alison Mozer 2023 <br />
         Todos os direitos reservados.
       </footer>
     </>
